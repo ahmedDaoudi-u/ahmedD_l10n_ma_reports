30685abbc4bf8b141a063c391f4adefeb9234786
Ahmed-Daoudi <107957529+ahmedDaoudi-u@users.noreply.github.com>
2024-02-14 16:13:41 +0100

    This is an update to that the modul would now use the inheritance

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Accounting Report",
+    "version": "17.0.1.0.0",
+    "website": "https://www.report.com",
+    "author": "ahmed",
+    "description": "THis Is A Project For Payslip Reporting",
+    "data": [
+        'security/ir.model.access.csv',
+        'views/payslip_inheritance.xml',
+    ],
+    "category": "",
+    "depends": ["l10n_ma_hr_payroll"],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
---
+++ models/hr_payslip.py
@@ -0,0 +1,8 @@
+from odoo import models, fields ,api
+
+class HrPayslip(models.Model):
+
+    _inherit = "hr.payslip"
+
+
+
