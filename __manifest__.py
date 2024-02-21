a34f919a32df6e35b135681c48fe22e328d45f33
ahmedDaoudi-u <moliti129@gmail.com>
2024-02-21 17:07:16 +0100

    File names updates with libraries and some xml files are added

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,15 @@
 {
-    "name": "Accounting Report",
+    "name": "Teos - Accounting Report",
     "version": "17.0.1.0.0",
     "website": "https://www.report.com",
-    "author": "ahmed",
-    "description": "THis Is A Project For Payslip Reporting",
+    "summary": "Accounting reports for Teos",
+    'category': 'Customizations/Custom',
+    "author": "Ahmed",
+    "description": "This Is A Project For Payslip Reporting",
     "data": [
         'security/ir.model.access.csv',
-        'views/payslip_inheritance.xml',
+        'reports/payslip_inheritance_report.xml',
+        'data/paper_format_report.xml'
     ],
     "category": "",
     "depends": ["l10n_ma_hr_payroll"],
