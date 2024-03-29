061cb46908d97f3b993dc7c4df6b2d5f4f3e057b
ahmedDaoudi-u <moliti129@gmail.com>
2024-03-29 12:28:58 +0000

    Adding the first rules parameters

--- __manifest__.py
+++ __manifest__.py
@@ -13,9 +13,10 @@
         'data/paper_format_report.xml',
         'views/custom_employee_data.xml',
         "views/custom_time_off.xml",
-        "data/moroccan_structure.xml",
+        "data/moroccan_structure_rules.xml",
         "data/moroccan_rules.xml",
-        "data/moroccan_category_data.xml"
+        "data/moroccan_category_data.xml",
+        "data/moroccan_salary_rules.xml"
     ],
     "category": "",
     "depends": ['l10n_ma_hr_payroll','hr_payroll', 'l10n_ma','hr_holidays'],
