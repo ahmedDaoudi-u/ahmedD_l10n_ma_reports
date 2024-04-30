5b7d8ad91b884304e206ba0bc52295b9f53542b5
ahmedDaoudi-u <moliti129@gmail.com>
2024-04-30 09:15:38 +0100

    adding the onchnage function to the hr_employee module

--- models/hr_company.py
+++ models/hr_company.py
@@ -11,16 +11,7 @@ class HrCompanydata(models.Model):
         String="Secteur d'activité")
 
 
-    @api.onchange('company_status')
-    def onchange_company_status(self):
-        employees = self.env['hr.employee'].search([])
-        hours = employees.mapped('resource_calendar_id')
-        status = self.company_status
 
-        if status == "Agricole":
-            hours.name = "Heures de travail par défaut à 44 heures par semaine"
-        else:
-            hours.name = "Heures de travail par défaut à 48 heures par semaine"
 
 
 
